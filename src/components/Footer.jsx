2c74813d07137ebded9dc95595cde669e4d6ac85
Wes <wes@miragecorp.org>
2025-04-22 09:52:23 -0500

    design: sizing on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,19 @@ import { NavLink } from "react-router-dom";
 const Footer = () => {
     return (
         <footer className="bg-foreground border-t border-border py-5 px-5 md:px-20 flex justify-between items-center">
-            <div className="text-sm text-copy-light">
+            <div className="text-sm font-light">
                 © 2025 Wes. All rights reserved.
             </div>
-            <div className="flex gap-5">
-                <NavLink to="/" className="text-copy-lighter hover:text-copy duration-200 transition ease-in-out">
+            <div className="flex gap-5 text-sm">
+                <NavLink to="/" className="hover:cursor-pointer hover:text-copy/70 transition-colors duration-200 ease-in-out">
                     Home
                 </NavLink>
-                <NavLink to="/portfolio" className="text-copy-lighter hover:text-copy duration-200 transition ease-in-out">
+                <NavLink to="/portfolio" className="hover:cursor-pointer hover:text-copy/70 transition-colors duration-200 ease-in-out">
                     Portfolio
                 </NavLink>
-                {/* <NavLink to="/about" className="text-copy-lighter hover:text-copy duration-200 transition ease-in-out">
-                    About
+                <NavLink to="/contact" className="hover:cursor-pointer hover:text-copy/70 transition-colors duration-200 ease-in-out">
+                    Contact
                 </NavLink>
-                <NavLink to="/faq" className="text-copy-lighter hover:text-copy duration-200 transition ease-in-out">
-                    FAQ
-                </NavLink> */}
             </div>
         </footer>
     );
