a9d2a6d424473df41316cdb761ef1b2ad373b5f8
Wes <wes@miragecorp.org>
2025-05-13 08:51:02 -0500

    feature: mobile support

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -27,18 +27,18 @@ const Contact = () => {
                 
                 <p className="text-lg max-w-2xl">Connect with me today to begin crafting your digital vision. I respond to all inquiries within 24 hours and can't wait to help bring your project to life!</p>
 
-                <div className="flex w-full flex-row items-center mt-8 bg-foreground p-4 rounded-lg drop-shadow-sm border-2 border-border gap-5">
-                    <a href="mailto:wes@mars.rip" className="bg-blue-500 text-white hover:scale-105 w-1/2 flex flex-col items-center drop-shadow-md rounded-lg py-7 transition-all duration-200">
+                <div className="flex w-full flex-col md:flex-row items-center mt-8 bg-foreground p-4 rounded-lg drop-shadow-sm border-2 border-border gap-5">
+                    <a href="mailto:wes@mars.rip" className="bg-blue-500 text-white hover:scale-105 w-full md:w-1/2 flex flex-col items-center drop-shadow-md rounded-lg py-7 transition-all duration-200">
                         <FontAwesomeIcon icon={faEnvelope} className="text-4xl" />
                         <p className="text-lg">wes@mars.rip</p>
                     </a>
 
-                    <a href="https://github.com/wtfchewy" className="bg-copy text-white hover:scale-105 w-1/2 flex flex-col items-center drop-shadow-md rounded-lg py-7 transition-all duration-200">
+                    <a href="https://github.com/wtfchewy" className="bg-copy text-white hover:scale-105 w-full md:w-1/2 flex flex-col items-center drop-shadow-md rounded-lg py-7 transition-all duration-200">
                         <FontAwesomeIcon icon={faGithub} className="text-4xl" />
                         <p className="text-lg">wtfchewy</p>
                     </a>
 
-                    <button onClick={() => copyToClipboard()} className="bg-indigo-500 text-white hover:scale-105 hover:cursor-pointer w-1/2 flex flex-col items-center drop-shadow-md rounded-lg py-7 transition-all duration-200">
+                    <button onClick={() => copyToClipboard()} className="bg-indigo-500 text-white hover:scale-105 hover:cursor-pointer w-full md:w-1/2 flex flex-col items-center drop-shadow-md rounded-lg py-7 transition-all duration-200">
                         <FontAwesomeIcon icon={faDiscord} className="text-4xl" />
                         <p className="text-lg">wtfchewy</p>
                     </button>
